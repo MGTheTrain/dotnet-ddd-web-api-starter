9f37cabee8a33e6af7455f727c1dbcc66f8e52a3
Marvin Gajek <Marvin.Gajek1@web.de>
2024-05-31 18:07:47 +0200

    modify namespace

--- {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}.Persistence/DataAccess/SqlDbContext.cs
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}.Persistence/DataAccess/SqlDbContext.cs
@@ -1,4 +1,4 @@
-﻿namespace {{cookiecutter.project_slug}}.Persistence;
+﻿namespace {{cookiecutter.project_slug}}.Persistence.DataAccess;
 
 // This layer is only necessary if using DBContexts with ORMs like EF Core for SQL like database tables with a schema. Otherwise you can remove it along with {{cookiecutter.project_slug}}.PersistenceTest.
 
